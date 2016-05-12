7fae225a720a700405315ed6933322139412a780
Ben Krieger <blitzrk@gmail.com>
2016-05-12 15:20:23 -0600

    Modularize elm code

--- web/elm/TwoBudget.elm
+++ web/elm/TwoBudget.elm
@@ -9,6 +9,7 @@ import Html.Events exposing (on, onInput, onClick)
 import Result
 import String
 import Task
+import Transaction exposing (Transaction)
 import WebSocket
 
 
@@ -43,27 +44,6 @@ type alias BudgetItem =
 type alias BudgetMonth = List BudgetItem
 
 
-type alias Transaction =
-  { amount   : String
-  , payee    : String
-  , category : String
-  , account  : String
-  , date     : Date.Date
-  , note     : String
-  , open     : Bool
-  , error    : String
-  }
-
-
-type TransactionField
-  = Amount
-  | Payee
-  | Category
-  | Account
-  | Date
-  | Note
-
-
 type alias Model =
   { user            : Maybe { name : String, jwt : String }
   , date            : Date.Date
@@ -72,7 +52,7 @@ type alias Model =
   , monthCache      : List (Int, BudgetMonth)
   , transaction     : Transaction
   }
-    
+
 
 init : ( Model, Cmd Msg )
 init =
@@ -84,7 +64,7 @@ init =
       , state = Overview
       , monthFocus = -1
       , monthCache = []
-      , transaction = newTransaction (Date.fromTime 0)
+      , transaction = Transaction.new (Date.fromTime 0)
       }
   in
     (default, Cmd.none)
@@ -97,7 +77,7 @@ init =
 type Msg
   = ToggleTransaction
   | InitTransaction Date.Date
-  | InputTransaction TransactionField String
+  | InputTransaction Transaction.Field String
   | SubmitTransaction
   | SyncFrom String
 
@@ -121,7 +101,7 @@ update msg model =
             True  -> (model, Cmd.none)
             False ->
               let
-                t = newTransaction date
+                t = Transaction.new date
                 newModel =
                   { model |
                     date = date,
@@ -129,17 +109,17 @@ update msg model =
                   }
               in
                 (newModel, Cmd.none)
-        
+
         InputTransaction field value ->
-          let newModel = 
+          let newModel =
             { model |
-              transaction = updateTransaction field value model.transaction }
+              transaction = Transaction.update field value model.transaction }
           in (newModel, Cmd.none)
-        
+
         SubmitTransaction ->
           let
             transaction = model.transaction
-            
+
             validation =
               (True, "")
           in
@@ -148,7 +128,7 @@ update msg model =
                 ( { model |
                     transaction = { transaction | open = False }
                   }, WebSocket.send (wsAddr name jwt) jsonForm )
-                  
+
               (False, msg) ->
                 ( { model |
                     transaction = { transaction | error = msg }
@@ -157,33 +137,6 @@ update msg model =
         SyncFrom jsonForm ->
           -- decode json, determine which fields to update, modify model
           (model, Cmd.none)
-
-
-updateTransaction : TransactionField -> String -> Transaction -> Transaction
-updateTransaction field value transaction =
-  case field of
-    Amount   -> {transaction | amount = value}
-    Payee    -> {transaction | payee = value}
-    Category -> {transaction | category = value}
-    Account  -> {transaction | account = value}
-    Note     -> {transaction | note = value}
-    Date ->
-      case Result.toMaybe <| Date.fromString value of
-        Nothing   -> transaction
-        Just date -> {transaction | date = date} -- Remember to validate date before syncing!
-
-
-newTransaction : Date.Date -> Transaction
-newTransaction date =
-  { amount   = ""
-  , payee    = ""
-  , category = ""
-  , account  = ""
-  , date     = date
-  , note     = ""
-  , open     = False
-  , error    = ""
-  }
 
 
 
@@ -207,76 +160,19 @@ subscriptions model =
 
 (=>) = (,)
 
-  
+
 view : Model -> Html Msg
 view model =
   div []
     [ div [] [ text "Hello World" ],
       button [ onClick ToggleTransaction ] [ text (if model.transaction.open then "-" else "+") ],
-      if model.transaction.open then transactionForm InputTransaction model.transaction else noElement
+      if model.transaction.open then
+        Transaction.form InputTransaction SubmitTransaction model.transaction
+      else
+        noElement
     ]
 
 
 noElement : Html Msg
 noElement =
   div [ style [ "display" => "none" ] ] []
-
-
-dateString : Date.Date -> String
-dateString date =
-  let month = case Date.month date of
-    Date.Jan -> 1
-    Date.Feb -> 2
-    Date.Mar -> 3
-    Date.Apr -> 4
-    Date.May -> 5
-    Date.Jun -> 6
-    Date.Jul -> 7
-    Date.Aug -> 8
-    Date.Sep -> 9
-    Date.Oct -> 10
-    Date.Nov -> 11
-    Date.Dec -> 12
-  in
-  [ Date.year date, month, Date.day date + 1 ]
-    |> List.map toString
-    |> List.map (\s -> if String.length s < 2 then "0" ++ s else s)
-    |> String.join "-"
-
-
-transactionForm : (TransactionField -> String -> Msg) -> Transaction -> Html Msg
-transactionForm tagger {amount, payee, category, account, date, note} =
-  let
-    updateInput field = onInput <| tagger field
-  in
-    -- TODO: Change to form element
-    H.form []
-      [ tr []
-        [ td [] [ label [] [ text "Amount: " ] ]
-        , td [] [ input [type' "number", updateInput Amount, value amount] [] ]
-        ]
-      , tr []
-        [ td [] [ label [] [ text "Payee: " ] ]
-        , td [] [ input [updateInput Payee, value payee] [] ]
-        ]
-      , tr []
-        [ td [] [ label [] [ text "Category: " ] ]
-        , td [] [ input [updateInput Category, value category] [] ]
-        ]
-      , tr []
-        [ td [] [ label [] [ text "Account: " ] ]
-        , td [] [ input [updateInput Account, value account] [] ]
-        ]
-      , tr []
-        [ td [] [ label [] [ text "Date: " ] ]
-        , td [] [ input [type' "date", updateInput Date, value <| dateString date] [] ]
-        ]
-      , tr []
-        [ td [] [ label [] [ text "Note: " ] ]
-        , td [] [ input [updateInput Note, value note] [] ]
-        ]
-      , tr [] [ button [onClick SubmitTransaction] [text "Submit"] ]
-      ]
-
-
-
---
+++ web/elm/src/Transaction.elm
@@ -0,0 +1,118 @@
+module Transaction exposing (..)
+
+import Date
+import Html as H exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (on, onInput, onClick)
+import String
+import WebSocket
+
+
+
+type alias Transaction =
+  { amount   : String
+  , payee    : String
+  , category : String
+  , account  : String
+  , date     : Date.Date
+  , note     : String
+  , open     : Bool
+  , error    : String
+  }
+
+
+type Field
+  = Amount
+  | Payee
+  | Category
+  | Account
+  | Date
+  | Note
+
+
+update : Field -> String -> Transaction -> Transaction
+update field value transaction =
+  case field of
+    Amount   -> {transaction | amount = value}
+    Payee    -> {transaction | payee = value}
+    Category -> {transaction | category = value}
+    Account  -> {transaction | account = value}
+    Note     -> {transaction | note = value}
+    Date ->
+      case Result.toMaybe <| Date.fromString value of
+        Nothing   -> transaction
+        Just date -> {transaction | date = date} -- Remember to validate date before syncing!
+
+
+new : Date.Date -> Transaction
+new date =
+  { amount   = ""
+  , payee    = ""
+  , category = ""
+  , account  = ""
+  , date     = date
+  , note     = ""
+  , open     = False
+  , error    = ""
+  }
+
+
+
+dateString : Date.Date -> String
+dateString date =
+  let month = case Date.month date of
+    Date.Jan -> 1
+    Date.Feb -> 2
+    Date.Mar -> 3
+    Date.Apr -> 4
+    Date.May -> 5
+    Date.Jun -> 6
+    Date.Jul -> 7
+    Date.Aug -> 8
+    Date.Sep -> 9
+    Date.Oct -> 10
+    Date.Nov -> 11
+    Date.Dec -> 12
+  in
+  [ Date.year date, month, Date.day date + 1 ]
+    |> List.map toString
+    |> List.map (\s -> if String.length s < 2 then "0" ++ s else s)
+    |> String.join "-"
+
+
+form : (Field -> String -> msg) -> msg -> Transaction -> Html msg
+form tagInput tagSubmit {amount, payee, category, account, date, note} =
+  let
+    updateInput field = onInput <| tagInput field
+  in
+    -- TODO: Change to form element
+    H.form []
+      [ tr []
+        [ td [] [ label [] [ text "Amount: " ] ]
+        , td [] [ input [type' "number", updateInput Amount, value amount] [] ]
+        ]
+      , tr []
+        [ td [] [ label [] [ text "Payee: " ] ]
+        , td [] [ input [updateInput Payee, value payee] [] ]
+        ]
+      , tr []
+        [ td [] [ label [] [ text "Category: " ] ]
+        , td [] [ input [updateInput Category, value category] [] ]
+        ]
+      , tr []
+        [ td [] [ label [] [ text "Account: " ] ]
+        , td [] [ input [updateInput Account, value account] [] ]
+        ]
+      , tr []
+        [ td [] [ label [] [ text "Date: " ] ]
+        , td [] [ input [type' "date", updateInput Date, value <| dateString date] [] ]
+        ]
+      , tr []
+        [ td [] [ label [] [ text "Note: " ] ]
+        , td [] [ input [updateInput Note, value note] [] ]
+        ]
+      , tr [] [ button [onClick tagSubmit] [text "Submit"] ]
+      ]
+
+
+
